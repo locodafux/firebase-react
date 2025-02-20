48f816e3c2dbc4208a4675b88ab48b32d59b8158
Leonardo Timkang <leonardo_timkangjr.it@wilcon.com.ph>
2025-02-20 08:11:23 +0800

    switched to tailwind

--- frontend/src/Pages/Login.jsx
+++ frontend/src/Pages/Login.jsx
@@ -1,97 +1,45 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function Login() {
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-    return (
-        <>
-       <div className="container">
-            <div className="card">
-                <div className="header">
-                    <span>Login</span>
-                </div>
-                <div className="content">   
-                    <div className="input-group">
-                        <span className="label">Email</span>
-                        <input className="text-input" type="text" value={email} onChange={(e) => setEmail(e.target.value)} required />
-                    </div>
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-                <div className="input-group">
-                        <span className="label">Password</span>
-                        <input className="text-input" value={password} onChange={(e)=>setPassword(e.target.value)} type="password" required />
-                    </div>
-                
-                <div className="input-group">
-                    <button className="primary-button">Login</button>
-                </div>
-
-                </div>
-                    
-
-            </div>
-        </div> 
-        <style>
-            {css}
-        </style>
-        </>
-    );
+  return (
+    <div className="h-screen w-screen bg-gray-100 flex justify-center items-center">
+      <div className="w-96 bg-white rounded-lg shadow-lg">
+        <div className="p-4 border-b border-gray-200">
+          <span className="text-sm font-semibold">Login</span>
+        </div>
+        <div className="p-8 flex flex-col items-center">
+          <div className="w-full mb-4">
+            <label className="text-xs text-gray-600">Email</label>
+            <input
+              className="text-sm w-full h-8 rounded-md border border-gray-400 mt-1 px-2 focus:outline-none focus:ring-2 focus:ring-gray-600"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="w-full mb-4">
+            <label className="text-sm text-gray-600">Password</label>
+            <input
+              className="text-xs w-full h-8 rounded-md border border-gray-400 mt-1 px-2 focus:outline-none focus:ring-2 focus:ring-gray-600"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <div className="w-full">
+            <button className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-200 hover:text-black transition">
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
-const css = `
-    .container {
-        height: 100vh;
-        width: 100vw;
-        background-color: #f8f8f8;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
 
-    .card {
-        width: 450px;
-        border-radius: .5rem;
-        background-color: #ffffff;
-    }
-
-    .header {
-        padding: .5rem;
-        border-bottom: 1px solid #f1f1f1;
-        }
-    .content {
-        padding: 2rem;
-        display: flex;
-        flex-direction: column;
-        align-items:center;
-    }
-
-    .text-input {
-        width: 100%;
-        height: 2rem;
-        border-radius: 5px;
-        border: 1px solid gray;
-        margin-top: 6px;
-        padding: 0 4px;
-    }
-
-    .input-group {
-        width:100%;
-        margin-bottom: 8px;
-    }
-    
-    .label {
-        font-size: 12px;
-    }
-    
-    .primary-button {
-        background-color: #0a0a0a;
-        padding: 8px 16px;
-        color: #ffffff;
-        border: none;
-        border-radius: 5px;
-    }
-    .primary-button:hover {
-        background-color: #f1f1f1;
-        color: #0a0a0a;
-    }
-
-`
-export default Login
+export default Login;
