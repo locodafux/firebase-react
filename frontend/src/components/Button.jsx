1c2386b9097fb04ac4414386b47b392d4b444cdd
Leonardo Timkang <leonardo_timkangjr.it@wilcon.com.ph>
2025-02-20 10:36:03 +0800

    update

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,8 +1,8 @@
-const PrimaryButton = ({text}) => {
+const PrimaryButton = ({ text }) => {
     return (
         <>
             <button  className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-200 hover:text-black transition">
-               {text} 
+               { text } 
             </button>
         </>
     )
